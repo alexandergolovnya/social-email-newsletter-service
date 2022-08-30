80950cbe1366f58d40a208755d15428b5286b431
Alexander Golovnya <aleksandergolovnya@gmail.com>
2022-08-31 00:35:53 +0300

    Initial project setup with Spring & Docker

---
+++ social-newsletter-outer-api/src/main/java/com/golovnya/social/newsletter/OuterApiApplication.java
@@ -0,0 +1,13 @@
+package com.golovnya.social.newsletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuterApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuterApiApplication.class, args);
+    }
+
+}
